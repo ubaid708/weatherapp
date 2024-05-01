1c5a7cfb3cb3bfd66001c95dd398aa0620522623
ubaid708 <168576727+ubaid708@users.noreply.github.com>
2024-05-01 09:14:48 +0500

    Add files via upload

---
+++ callingApi.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CallingApi extends StatefulWidget {
+  const CallingApi({super.key});
+
+  @override
+  State<CallingApi> createState() => _CallingApiState();
+}
+
+class _CallingApiState extends State<CallingApi> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+  getData(){
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
